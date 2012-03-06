65f42e7b4e1cd93504f8091f10e6fa1294cca986
Andrey Doroshenko <dorosshh@gmail.com>
2012-03-07 00:27:32 +0200

    add drag and drop in blog-admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,14 @@ from blog.models import BlogPost
 
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'text', 'date', 'active', 'sorting', )
+    list_editable = ('sorting', )
+
+    class Media:
+        js = (
+            '/static/js/jquery-1.7.1.min.js',
+            '/static/js/jquery-ui-1.8.18.custom.min.js',
+            '/static/js/blog/admin_list_reorder.js',
+        )
+
 
 admin.site.register(BlogPost, BlogPostAdmin)
